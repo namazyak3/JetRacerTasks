349bbc286617a1d64c49f6d55a331e22a3bd004f
namazyak3 <namazyake@gmail.com>
2025-06-14 18:39:44 +0900

    [#15 Test] Unity env

---
+++ MLAgents/unity_env_test.py
@@ -0,0 +1,18 @@
+from mlagents_envs.environment import UnityEnvironment
+from mlagents_envs.envs.unity_gym_env import UnityToGymWrapper
+
+unity_env = UnityEnvironment(
+    file_name="Builds/Linux/Shape8-144.x86_64",
+    base_port=5005,
+    worker_id=0,
+    seed=123,
+    additional_args=["--no-graphics"]
+)
+env = UnityToGymWrapper(unity_env=unity_env, flatten_branched=True)
+
+try:
+    obs = env.reset()
+    print(obs.shape)
+
+except KeyboardInterrupt:
+    env.close()
