6a4ffa79cb6f0f3dc3f543737ff9d969fd18f22e
namazyak3 <namazyake@gmail.com>
2025-06-17 00:10:34 +0900

    [#24 Update] Env

--- Assets/Scripts/EnvSettings.cs
+++ Assets/Scripts/EnvSettings.cs
@@ -7,7 +7,7 @@ public class EnvSettings : MonoBehaviour
     public void Start()
     {
         // 物理演算の学習ステップ
-        Time.fixedDeltaTime = 0.05f;
+        Time.fixedDeltaTime = 0.02f;
 
         // フレーム制限の解除
         QualitySettings.vSyncCount = 0;
@@ -18,5 +18,8 @@ public class EnvSettings : MonoBehaviour
 
         // ウィンドウサイズの削減
         Screen.SetResolution(84, 84, false);
+
+        // その他
+        Application.runInBackground = true;
     }
 }
