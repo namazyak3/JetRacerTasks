e21758949344be8e924f7ba749e1c3f8a9752caf
namazyak3 <namazyake@gmail.com>
2025-06-18 22:09:00 +0900

    [#31 Update] Train

--- Assets/Scripts/EnvSettings.cs
+++ Assets/Scripts/EnvSettings.cs
@@ -7,7 +7,7 @@ public class EnvSettings : MonoBehaviour
     public void Start()
     {
         // 物理演算の学習ステップ
-        Time.fixedDeltaTime = 0.02f;
+        Time.fixedDeltaTime = 0.05f;
 
         // フレーム制限の解除
         QualitySettings.vSyncCount = 0;
@@ -17,7 +17,7 @@ public class EnvSettings : MonoBehaviour
         QualitySettings.SetQualityLevel(0);
 
         // ウィンドウサイズの削減
-        Screen.SetResolution(84, 84, false);
+        Screen.SetResolution(160, 120, false);
 
         // その他
         Application.runInBackground = true;
